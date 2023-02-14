ac9ae8c9bf5cd1460929ac880c625b1bc3778d0d
Cristin <anita_kineva@mail.ru>
2023-02-14 11:36:54 +0300

    added program Get third number

--- Get_third_number/Program.cs
+++ Get_third_number/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Получить третью цифру любого числа
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+char[] arrnumber = number.ToCharArray();
+
+if (arrnumber.Length < 3)
+{
+    Console.Write($"Третья цифра у числа {number} отсутсвует.");
+}
+else if (arrnumber[0].Equals('0'))
+{
+    int i = 1;
+    while (arrnumber[i].Equals('0'))
+    {
+        i++;
+    }
+    Console.Write($"Третья цифра у числа {number}: {arrnumber[i + 2]}.");
+}
+else if (arrnumber[0].Equals('-'))
+{
+    int i = 1;
+    while (arrnumber[i].Equals('-'))
+    {
+        i++;
+    }
+    while (arrnumber[i].Equals('0'))
+    {
+        i++;
+    }
+    Console.Write($"Третья цифра у числа {number}: {arrnumber[i + 2]}.");
+}
+else
+{
+    Console.Write($"Третья цифра числа {number}: {arrnumber[2]}.");
+}
