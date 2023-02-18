ea95d75a0f3ad344a6c29cd8b013f1d646c0051e
Cristin <anita_kineva@mail.ru>
2023-02-18 22:06:50 +0300

    added Goooogle

---
+++ Goooogle/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите функцию, которая принимает число и возвращает 
+// слово Google с количеством букв o, равным этому числу.
+
+Console.Write("Введите число: ");
+int N = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+string G = new string ('o', N);
+
+Console.WriteLine($"G{G}gle");
