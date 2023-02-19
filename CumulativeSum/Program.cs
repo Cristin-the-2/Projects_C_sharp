75a0e97a57d703276446aac17403734be3c4e715
Cristin <anita_kineva@mail.ru>
2023-02-19 21:40:48 +0300

    added CumulativeSum

---
+++ CumulativeSum/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите функцию, которая принимает массив чисел и возвращает массив, 
+// в котором каждое число — это число из исходного массива 
+// + сумма всех предыдущих чисел из того же массива.
+
+int[] AskArr()
+{
+    Console.Write("Введите массив через пробел: ");
+    string? str = Console.ReadLine();
+    int[] array = str.Split(' ').Select(s => Int32.Parse(s)).ToArray();
+    return array;
+}
+
+void PrintArr(int[] A)
+{
+    Console.WriteLine("[" + string.Join(",", A) + "]");
+}
+
+int[] arr = AskArr();
+PrintArr(arr);
+
+int sum = 0;
+int[] arr1 = new int[arr.Length];
+for (int i = 0; i < arr.Length; i++)
+{
+    sum = sum + arr[i];
+    arr1[i] = sum;
+}
+PrintArr(arr1);
