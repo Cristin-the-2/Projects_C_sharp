42ce0b2bf0406f530307d1491cb886682f6d9656
Cristin <anita_kineva@mail.ru>
2023-02-19 11:10:48 +0300

    added IsIsogam

---
+++ IsIsogram/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите функцию, которая принимает строку (слово) и возвращает True 
+// или False в зависимости от того, является слово изограмой или нет.
+
+Console.Write("Введите слово: ");
+string? str = Console.ReadLine();
+str = str.ToLower();
+char[] arrStr = str.ToCharArray();
+bool Result = false;
+
+for (int tmp = arrStr.Length-1; tmp > 0; tmp = tmp - 1)
+{
+    for (int i = tmp-1; i >= 0; i = i - 1)
+    {
+        if (arrStr[tmp] != arrStr[i])
+        {
+            Result = true;
+        }
+        else
+        {
+            Result = false;
+            goto LoopEnd;
+        }
+    }
+}
+
+LoopEnd: Console.WriteLine(Result);
